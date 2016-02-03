4b4678cf594f92d24aadf677eaccafc88f7afae0
Sylvain <sylvain.s.cloutier@cgi.com>
2016-02-03 10:28:21 -0500

    Added some documentation

--- SimpleDataFixLanguage/src/com/sdfl/code/SDFLGrammar.java
+++ SimpleDataFixLanguage/src/com/sdfl/code/SDFLGrammar.java
@@ -1,3 +1,22 @@
+/**
+ * This file is part of the Simple Data Fix Language (SDFL) core.
+ * 
+ * All components of the language (compiler, interpreter, etc.) are
+ * free and open source: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * SDFL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SDFL.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author Sylvain Cloutier
+ */
 package com.sdfl.code;
 
 import java.lang.reflect.Field;
@@ -6,8 +25,16 @@ import java.util.LinkedList;
 import java.util.List;
 
 
+/**
+ * This class is an abstract class that uses reflection to find the declared fields of
+ * its subclasses. Those fields contains the different reserved keywords for the language.
+ * 
+ * @see SDFLDelimiters
+ * @see SDFLKeywords
+ * @see SDFLSyntax 
+ * @author Sylvain Cloutier
+ */
 public abstract class SDFLGrammar {
-
 	protected static List<String> valuesOf(Class<? extends SDFLGrammar> pGrammarClass) {
 		List<String> lValues = new LinkedList<>();
 		for (Field lCurField : pGrammarClass.getDeclaredFields()) {
--- SimpleDataFixLanguage/src/com/sdfl/code/SDFLKeywords.java
+++ SimpleDataFixLanguage/src/com/sdfl/code/SDFLKeywords.java
@@ -1,7 +1,33 @@
+/**
+ * This file is part of the Simple Data Fix Language (SDFL) core.
+ * 
+ * All components of the language (compiler, interpreter, etc.) are
+ * free and open source: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * SDFL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SDFL.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author Sylvain Cloutier
+ */
 package com.sdfl.code;
 
 import java.util.List;
 
+/**
+ * Keywords holder class. This class should only contain static Strings with
+ * all lowercase reserved keywords of the language.
+ * 
+ * @see SDFLGrammar
+ * @author Sylvain Cloutier
+ */
 public abstract class SDFLKeywords extends SDFLGrammar {
 	public static final String IN = "in";
 	public static final String PACKAGE = "package";
